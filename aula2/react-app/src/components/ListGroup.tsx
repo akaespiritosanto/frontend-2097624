298b8ec55924b466ac513a769d8f6218967e5d58
akaespiritosanto <2097624@student.uma.pt>
2025-02-25 20:45:05 +0000

    wef
    weffew

--- aula2/react-app/src/components/ListGroup.tsx
+++ aula2/react-app/src/components/ListGroup.tsx
@@ -1,19 +1,27 @@
 import { Fragment } from "react/jsx-runtime";
+import { MouseEvent } from "react";
 
-const localidades = ["Funchal","Santa Cruz","Ribeira Brava"]
+var localidades = ["Funchal", "Santa Cruz", "Santana"];
+
+//localidades = [];
+
+const validateArray = () =>  {return localidades.length === 0 ? <h1>O array esta vazio</h1> : null}; 
+
+const getMessage = (event:MouseEvent) => {
+    console.log(event);
+};
 
 function ListGroup(){
     return (
-        <>
-            <ul className="list-group">{
-                localidades.map((item) => (
-                <li key=(item)>(item)</li>
-            )}
-            </ul>
-        </>
+    <Fragment>
+        <h1>List Group</h1>
+        {validateArray()}
+        <ul className="list-group" key={} onClick={getMessage}>
+            {
+                localidades.map((item)=>(<li key={item}>{item}</li>))
+            }
+        </ul>
+    </Fragment>
     );
 }
-
 export default ListGroup
-
-// está mal a função ListGroup
