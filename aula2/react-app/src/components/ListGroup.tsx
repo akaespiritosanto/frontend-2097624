2756fe12a70eaaecfba73a19fa689140ae28fa1b
akaespiritosanto <2097624@student.uma.pt>
2025-03-11 21:09:12 +0000

    sfef
    fsdf

--- aula2/react-app/src/components/ListGroup.tsx
+++ aula2/react-app/src/components/ListGroup.tsx
@@ -1,27 +1,38 @@
 import { Fragment } from "react/jsx-runtime";
 import { MouseEvent } from "react";
 
-var localidades = ["Funchal", "Santa Cruz", "Santana"];
+let localidades = ["Funchal", "Santa Cruz", "Santana"];
+const getMessage = () => {return localidades.length === 0 ? <p>No item found</p>:null};
 
-//localidades = [];
-
-const validateArray = () =>  {return localidades.length === 0 ? <h1>O array esta vazio</h1> : null}; 
-
-const getMessage = (event:MouseEvent) => {
-    console.log(event);
-};
+//Event handler
+const handleClick = (event: MouseEvent) => (console.log(event));
 
 function ListGroup(){
-    return (
-    <Fragment>
-        <h1>List Group</h1>
-        {validateArray()}
-        <ul className="list-group" key={} onClick={getMessage}>
-            {
-                localidades.map((item)=>(<li key={item}>{item}</li>))
-            }
-        </ul>
-    </Fragment>
+    return(
+    <>
+     <h1>Título da lista</h1>
+     {getMessage()}
+     <ul className="list-group">
+        {localidades.map((item) => (<li className="list-group-item" key={item} onClick={handleClick}>{item}</li>))}
+     </ul>
+    </>
     );
 }
-export default ListGroup
+
+function ListGroup2(props: ListGroupProps){
+    const  [selectedindex, setSelectedIndex] = useState(-1);
+    return (
+        <>
+        <h1>{props.title}</h1>
+        <ul className="list-group">
+            {props.cities.map((item, index) =>(
+            <li className={selectedindex === index ? "list-group-item active" : "list-group-item"}key={item}
+            onClick={() => {setSelectedIndex(index);}}
+            >
+                {item}
+            </li>))}
+        </ul>
+</>);}
+
+export default ListGroup2;
+export default ListGroup;
